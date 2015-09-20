238a90e361ce2094be6962e162627a652d71d586
Azat Almeev <azat.almeev@gmail.com>
2015-09-20 23:31:58 +0300

    Initial Commit

---
+++ Lesson1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson1
+//
+//  Created by Azat Almeev on 20.09.15.
+//  Copyright © 2015 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lesson1
+//
+//  Created by Azat Almeev on 20.09.15.
+//  Copyright © 2015 Azat Almeev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
